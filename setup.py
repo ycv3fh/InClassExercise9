e06223d823e742b3d16446209c72b05cdc37af75
Clarissa_Benitez <ycv3fh@Virginia.edu>
2024-03-26 20:23:43 -0400

    Files for demo package created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = "InClassExercise9",
+    version = "0.1",
+    description = "A simple demo package",
+    url = "https://github.com/ycv3fh/InClassExercise9",
+    author = "Clarissa Benitez",
+    license = "MIT",
+    packages = ['DemoPackage']
+)
